d3352f5e0f16ed049883597b232814bb055c6931
Caio Vinícius <caiooom91@gmail.com>
2023-02-01 15:54:59 -0300

    feat: Cria classe Dwarf

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  static instances = 0;
+  _maxLifePoints = 80;
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    super(name, dexterity);
+    Dwarf.instances += 1;
+    this._maxLifePoints = 80;
+  }
+
+  get maxLifePoints(): number { return this._maxLifePoints; }
+
+  static createdRacesInstances() { return this.instances; }
+}
+
+export default Dwarf;
